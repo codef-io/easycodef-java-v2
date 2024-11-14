30966fabef9a262fb5ff419d4701c8137c8de38b
h-beeen <happybean@hectodata.co.kr>
2024-11-14 10:01:54 +0900

    feat: httpClient5 호출부 로직 각 클래스 단위로 분리

--- src/main/java/io/codef/api/EasyCodefConnector.java
+++ src/main/java/io/codef/api/EasyCodefConnector.java
@@ -1,8 +1,6 @@
 package io.codef.api;
 
-
 import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
 import io.codef.api.constants.CodefHost;
 import io.codef.api.constants.CodefPath;
 import io.codef.api.dto.EasyCodefRequest;
@@ -12,25 +10,13 @@ import io.codef.api.error.CodefException;
 import io.codef.api.util.HttpClientUtil;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.core5.http.ClassicHttpResponse;
-import org.apache.hc.core5.http.HttpStatus;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 
-import java.io.IOException;
-import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
-import static io.codef.api.dto.EasyCodefRequest.*;
-import static io.codef.api.dto.EasyCodefResponse.DATA;
-import static io.codef.api.dto.EasyCodefResponse.RESULT;
+import static io.codef.api.dto.EasyCodefRequest.BASIC_TOKEN_FORMAT;
+import static io.codef.api.dto.EasyCodefRequest.BEARER_TOKEN_FORMAT;
 import static org.apache.hc.core5.http.HttpHeaders.AUTHORIZATION;
-
-@FunctionalInterface
-interface ResponseProcessor<T> {
-    T process(ClassicHttpResponse response) throws IOException, ParseException;
-}
 
 public final class EasyCodefConnector {
     private static final ResponseHandler responseHandler = new ResponseHandler();
@@ -39,22 +25,18 @@ public final class EasyCodefConnector {
         throw new IllegalStateException("Utility class");
     }
 
-    /**
-     * OAuth 토큰을 발급받습니다.
-     */
-    public static String requestToken(String codefOAuthToken) {
+    public static String requestToken(
+            String codefOAuthToken
+    ) throws CodefException {
         HttpPost request = createTokenRequest(codefOAuthToken);
         return executeRequest(request, responseHandler::handleTokenResponse);
     }
 
-    /**
-     * CODEF 상품 API를 호출합니다.
-     */
     public static EasyCodefResponse requestProduct(
             EasyCodefRequest request,
             EasyCodefToken token,
             String requestUrl
-    ) {
+    ) throws CodefException {
         HttpPost httpRequest = createProductRequest(request, token, requestUrl);
         return executeRequest(httpRequest, responseHandler::handleProductResponse);
     }
@@ -79,7 +61,10 @@ public final class EasyCodefConnector {
         return httpPost;
     }
 
-    private static <T> T executeRequest(HttpPost request, ResponseProcessor<T> processor) {
+    private static <T> T executeRequest(
+            HttpPost request,
+            ResponseProcessor<T> processor
+    ) {
         try (CloseableHttpClient httpClient = HttpClientUtil.createClient()) {
             return httpClient.execute(request, processor::process);
         } catch (CodefException e) {
@@ -87,40 +72,5 @@ public final class EasyCodefConnector {
         } catch (Exception e) {
             throw CodefException.of(CodefError.INTERNAL_SERVER_ERROR, e);
         }
-    }
-}
-
-final class ResponseHandler {
-    String handleTokenResponse(ClassicHttpResponse response) throws IOException, ParseException {
-        final String responseBody = EntityUtils.toString(response.getEntity());
-
-        return switch (response.getCode()) {
-            case HttpStatus.SC_OK -> JSON.parseObject(responseBody).getString(ACCESS_TOKEN);
-            case HttpStatus.SC_UNAUTHORIZED -> throw CodefException.of(CodefError.OAUTH_UNAUTHORIZED, responseBody);
-            default -> throw CodefException.of(CodefError.OAUTH_INTERNAL_ERROR, responseBody);
-        };
-    }
-
-    EasyCodefResponse handleProductResponse(ClassicHttpResponse response) throws IOException, ParseException {
-        String httpResponse = EntityUtils.toString(response.getEntity());
-        String decodedResponse = URLDecoder.decode(httpResponse, StandardCharsets.UTF_8);
-
-        if (response.getCode() != HttpStatus.SC_OK) {
-            throw CodefException.of(CodefError.CODEF_API_SERVER_ERROR, decodedResponse);
-        }
-
-        return parseProductResponse(decodedResponse);
-    }
-
-    private EasyCodefResponse parseProductResponse(String decodedResponse) {
-        JSONObject jsonResponseObject = JSON.parseObject(decodedResponse);
-
-        EasyCodefResponse.Result resultResponse = jsonResponseObject.getJSONObject(RESULT)
-                .to(EasyCodefResponse.Result.class);
-
-        Object dataResponse = jsonResponseObject.getJSONObject(DATA)
-                .to(Object.class);
-
-        return new EasyCodefResponse(resultResponse, dataResponse);
     }
 }
